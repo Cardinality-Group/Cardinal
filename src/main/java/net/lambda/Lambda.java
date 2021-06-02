f9943ebe0a73fd888cf29895eed4d90924fb5264
Word <72238319+Office-Word@users.noreply.github.com>
2021-06-02 15:26:05 -0600

    Initial Commit

---
+++ src/main/java/net/lambda/Lambda.java
@@ -0,0 +1,13 @@
+package net.lambda;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Lambda implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
