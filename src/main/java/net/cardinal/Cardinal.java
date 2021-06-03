067473f58de015b5d302abed64aaf74a756f7e54
Word <72238319+Office-Word@users.noreply.github.com>
2021-06-03 07:02:15 -0600

    idk

--- src/main/java/net/cardinal/Cardinal.java
+++ src/main/java/net/cardinal/Cardinal.java
@@ -4,9 +4,13 @@ import net.fabricmc.api.ModInitializer;
 
 public class Cardinal implements ModInitializer {
 
+    public final static String name = "Cardinal", version = "1.02";
+
     @Override
     public void onInitialize() {
-        System.out.println("Booting Cardinal");
+        System.out.println("Starting " + name + " v" + version);
+        
+        
     }
 
 }
