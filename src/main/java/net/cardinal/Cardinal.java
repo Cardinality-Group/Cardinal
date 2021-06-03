b03b2c582bca9ccf0ed64d27fa5eecc543880530
Word <72238319+Office-Word@users.noreply.github.com>
2021-06-03 10:01:53 -0600

    Added a basic discord rpc <Todo: add in mixins>

--- src/main/java/net/cardinal/Cardinal.java
+++ src/main/java/net/cardinal/Cardinal.java
@@ -1,16 +1,23 @@
 package net.cardinal;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.*;
+
+import net.cardinal.discord.*;
 
 public class Cardinal implements ModInitializer {
 
     public final static String name = "Cardinal", version = "1.02";
+    public final static Cardinal cardinal = new Cardinal();
+    
+    public DiscordRP discordRP = new DiscordRP();
 
     @Override
     public void onInitialize() {
         System.out.println("Starting " + name + " v" + version);
-        
-        
+
+        discordRP.start();
     }
+
+    
 
 }
